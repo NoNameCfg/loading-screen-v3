e74b6a06f26f5cf91f696f47f2e8b74e61f37775
NoNameCfg <65350561+NoNameCfg@users.noreply.github.com>
2022-05-25 18:32:54 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+game 'gta5'
+
+author "F1"
+description "discord.gg/Nb23tC3wmA"
+version "3.0"
+
+files {
+    'index.html',
+    'audio/*.mp3',
+    'css/styles.css',
+    'video/*',
+    'img/*',
+    'img/*/*',
+    'js/main.js',
+    'video/video.mp4',
+}
+
+loadscreen 'index.html'
+loadscreen_cursor 'yes'
